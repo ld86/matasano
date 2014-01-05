4400fac429fb87fcaee2c2f15e8fd8b0ef53caea
Bogdan Melnik <ld@hackerdom.ru>
2014-01-06 01:03:19 +0600

    Print custom base64

--- task_1.cpp
+++ task_1.cpp
@@ -3,10 +3,16 @@
 #include "base64.h"
 #include "hex.h"
 
-int main() {
-  // I'm killing your brain like a poisonous mushroom
-  matasano::hex_t hex = matasano::string2hex("49276d206b696c6c696e6720796f75722062"
-                                             "7261696e206c696b65206120706f69736f6e"
-                                             "6f7573206d757368726f6f6d");
+int main(int argc, char** argv) {
+  matasano::hex_t hex;
+  if (argc == 2) {
+    hex = matasano::string2hex(argv[1]);
+  } else {
+    // I'm killing your brain like a poisonous mushroom
+    hex = matasano::string2hex("49276d206b696c6c696e6720796f75722062"
+                               "7261696e206c696b65206120706f69736f6e"
+                               "6f7573206d757368726f6f6d");
+  }
+
   std::cout << matasano::hex2base64(hex) << std::endl;
 }
